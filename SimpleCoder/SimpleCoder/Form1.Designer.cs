bba491fef8f0b1d4a903af93f94084a15f2debf0
A-sey <36642186+A-sey@users.noreply.github.com>
2018-04-26 00:18:09 +0300

    Add files via upload

---
+++ SimpleCoder/SimpleCoder/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace SimpleCoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.кодироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.декодироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.кодироватьToolStripMenuItem,
+            this.декодироватьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(631, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // кодироватьToolStripMenuItem
+            // 
+            this.кодироватьToolStripMenuItem.Name = "кодироватьToolStripMenuItem";
+            this.кодироватьToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.кодироватьToolStripMenuItem.Text = "Кодировать";
+            this.кодироватьToolStripMenuItem.Click += new System.EventHandler(this.кодироватьToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(20, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(586, 120);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(20, 196);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(586, 88);
+            this.textBox2.TabIndex = 2;
+            // 
+            // декодироватьToolStripMenuItem
+            // 
+            this.декодироватьToolStripMenuItem.Name = "декодироватьToolStripMenuItem";
+            this.декодироватьToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.декодироватьToolStripMenuItem.Text = "Декодировать";
+            this.декодироватьToolStripMenuItem.Click += new System.EventHandler(this.декодироватьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 311);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem кодироватьToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolStripMenuItem декодироватьToolStripMenuItem;
+    }
+}
+
---
+++ SimpleCoder/SimpleCoder/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void кодироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            //int mod = 0;
+            string temp = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] >= 32 && input[i] <= 64) { temp += (char)(input[i] - 31); }
+                if (input[i] >= 97 && input[i] <= 122) { temp += (char)(input[i] - 63); }
+                if (input[i] >= 65 && input[i] <= 90) { temp += (char)(60); temp += (char)(input[i] - 31); }
+            }
+            while (temp.Length % 4 != 0)
+                temp += (char)(1);
+
+            string output = "";
+            for (int i = 0; i < temp.Length; i += 4)
+            {
+                output += (char)(temp[i] << 2 | temp[i + 1] >> 4);
+                output += (char)(temp[i + 1] << 4 | temp[i + 2] >> 2);
+                output += (char)(temp[i + 2] << 6 | temp[i + 3]);
+            }
+            textBox2.Text = output;
+        }
+
+
+
+
+        private void декодироватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            string temp = "";
+            for(int i=0; i < input.Length; i += 3)
+            {
+                temp += (char)(input[i] >> 2);
+                temp += (char)(input[i] << 4 & 63 | input[i + 1] >> 4);
+                temp += (char)(input[i + 1] << 2 & 63 | input[i + 2] >> 6);
+                temp += (char)(input[i + 2] & 63);
+            }
+            string output="";
+            for (int i=0; i<temp.Length; i++)
+            {
+                if (temp[i] == 60)
+                {
+                    if (temp[i + 1] >= 34 && temp[i + 1] <= 59) { output += (char)(temp[i+1] + 31); i++; }
+                }
+                else
+                {
+                    if (temp[i] >= 1 && temp[i] <= 33) { output += (char)(temp[i] + 31); }
+                    if (temp[i] >= 34 && temp[i] <= 59) { output += (char)(temp[i] + 63); }
+                }
+            }
+            textBox2.Text = output;
+
+        }
+    }
+}
