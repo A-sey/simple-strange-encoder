08a1779e8521abc87f73fd25eba5e6b6ed78b1a4
Smartkin <vladislav5019@gmail.com>
2018-04-27 19:31:09 +0300

    Added console for debug configs

--- SimpleCoder/SimpleCoder/Form1.cs
+++ SimpleCoder/SimpleCoder/Form1.cs
@@ -8,14 +8,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace SimpleCoder
 {
     public partial class Form1 : Form
     {
+        UnicodeEncoding encoder;
+
         public Form1()
         {
             InitializeComponent();
+            encoder = new UnicodeEncoding(false, true, true); //Для разбития строки на байты
         }
 
         private void кодироватьToolStripMenuItem_Click(object sender, EventArgs e)
@@ -35,6 +39,14 @@ namespace SimpleCoder
             string temp = "";
             string missed = "";
             int mod = 0;
+            //Вывод байтов оригинального слова
+            byte[] original_bytes = encoder.GetBytes(input);
+            Console.Write("Original bytes: ");
+            foreach (byte b in original_bytes)
+            {
+                Console.Write(b + " ");
+            }
+            Console.WriteLine();
             // Проверяем каждый символ
             for (int i = 0; i < input.Length; i++)
             {
@@ -46,6 +58,7 @@ namespace SimpleCoder
                         if (mod == 0 && input[i] >= 'А') { mod = 1; temp += (char)(63); }
                         if (mod == 1 && input[i] < 'А') { mod = 0; temp += (char)(63); }
                     }
+                    
                     // Непосредственно кодирование
                     if (input[i] >= ' ' && input[i] <= '@') { temp += (char)(input[i] - 31); }
                     else if (input[i] >= 'a' && input[i] <= 'z') { temp += (char)(input[i] - 63); }
@@ -54,12 +67,15 @@ namespace SimpleCoder
                     else if (input[i] >= 'A' && input[i] <= 'Z') { temp += (char)(62); temp += (char)(input[i] - 31); }
                     else if (input[i] >= 'А' && input[i] <= 'Я') { temp += (char)(62); temp += (char)(input[i] - 1012); }
                 }
-                else { missed += input[i]; }
+                else //Добавляем в список пропущенных
+                {
+                    missed += input[i];
+                }
             }
             // Добавляем пробелы, чтобы число символов стало кратно четырём
             while (temp.Length % 4 != 0)
                 temp += (char)(1);
-            // Возвращаем результат
+            
             // Проверка на наличие потеряных символов
             if (missed.Length > 0)
             {
@@ -73,6 +89,15 @@ namespace SimpleCoder
                 textBox3.Visible = false;
                 textBox3.Text = "";
             }
+            //Вывод байтов закодированного слова
+            byte[] encoded_bytes = encoder.GetBytes(temp);
+            Console.Write("Encoded bytes: ");
+            foreach (byte b in encoded_bytes)
+            {
+                Console.Write(b + " ");
+            }
+            Console.WriteLine();
+            // Возвращаем результат
             return temp;
         }
 
@@ -87,6 +112,14 @@ namespace SimpleCoder
                 output += (char)(temp[i + 1] << 4 | temp[i + 2] >> 2);
                 output += (char)(temp[i + 2] << 6 | temp[i + 3]);
             }
+            //Вывод байтов сжатого закодированного слова
+            byte[] compressed_bytes = encoder.GetBytes(output);
+            Console.Write("Compressed bytes: ");
+            foreach (byte b in compressed_bytes)
+            {
+                Console.Write(b + " ");
+            }
+            Console.WriteLine();
             // Возвращаем результат
             return output;
         }
@@ -119,6 +152,14 @@ namespace SimpleCoder
                 temp += (char)(input[i + 1] << 2 & 63 | input[i + 2] >> 6);
                 temp += (char)(input[i + 2] & 63);
             }
+            //Вывод байтов расжатого закодированного слова
+            byte[] decompressed_bytes = encoder.GetBytes(temp);
+            Console.Write("Decompressed bytes: ");
+            foreach (byte b in decompressed_bytes)
+            {
+                Console.Write(b + " ");
+            }
+            Console.WriteLine();
             // Возвращаем то, что получилось
             return temp;
         }
@@ -135,9 +176,9 @@ namespace SimpleCoder
                 if (temp[i] == 62)
                 {
                     // Заглавные латиница
-                    if (temp[i + 1] >= 34 && temp[i + 1] <= 59 && mod==0) { output += (char)(temp[i + 1] + 31); i++; }
+                    if (temp[i + 1] >= 34 && temp[i + 1] <= 59 && mod == 0) { output += (char)(temp[i + 1] + 31); i++; }
                     // Заглавные Кириллица
-                    if (temp[i + 1] >= 28 && temp[i + 1] <= 59 && mod==1) { output += (char)(temp[i + 1] + 1012); i++; }
+                    if (temp[i + 1] >= 28 && temp[i + 1] <= 59 && mod == 1) { output += (char)(temp[i + 1] + 1012); i++; }
                 }
                 // Если не встретили символ, означающий заглавную букву
                 else
@@ -145,16 +186,40 @@ namespace SimpleCoder
                     // Символы до 8
                     if (temp[i] >= 1 && temp[i] <= 27) { output += (char)(temp[i] + 31); }
                     // Символы начиная с 8
-                    if (temp[i] >= 28 && temp[i] <= 33 && mod==0) { output += (char)(temp[i] + 31); }
+                    else if (temp[i] >= 28 && temp[i] <= 33 && mod == 0) { output += (char)(temp[i] + 31); }
                     // Буквы на латинице
-                    if (temp[i] >= 34 && temp[i] <= 59 && mod==0) { output += (char)(temp[i] + 63); }
+                    else if (temp[i] >= 34 && temp[i] <= 59 && mod == 0) { output += (char)(temp[i] + 63); }
                     // Буквы на Кириллице
-                    if (temp[i] >= 28 && temp[i] <= 59 && mod == 1) { output += (char)(temp[i] + 1044); }
+                    else if (temp[i] >= 28 && temp[i] <= 59 && mod == 1) { output += (char)(temp[i] + 1044); }
                     // P.S. Даже не спрашивай, откуда такие дикие числа
                 }
             }
+            //Вывод байтов раскодированного слова
+            byte[] decoded_bytes = encoder.GetBytes(output);
+            Console.Write("Decoded bytes: ");
+            foreach (byte b in decoded_bytes)
+            {
+                Console.Write(b + " ");
+            }
+            Console.WriteLine();
             // Возвращаем полученный текст
             return output;
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+#if DEBUG
+            //Создание консоли для отладки
+            AllocConsole();
+#endif
+        }
+//Если компилим в релиз конфигурации не привязывать консоль
+#if DEBUG
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+#endif
     }
 }
